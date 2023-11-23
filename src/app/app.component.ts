db9597d85a9c51ef4850c4d133321c225da7a220
sagarl10 <sagarsagu452@gmail.com>
2023-11-23 18:09:18 +0530

    employee-manager

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,76 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Employee } from './employee';
+import { EmployeeService } from './employee.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'employee-manager-system';
+export class AppComponent implements OnInit {
+  title(title: any) {
+    throw new Error('Method not implemented.');
+  }
+
+  public employees: Employee[]=[];
+  public editEmployee: Employee;
+  public deleteEmployee: Employee;
+  constructor(private employeeService:EmployeeService){
+    this.editEmployee={} as Employee;
+    this.deleteEmployee={} as Employee;
+    
+  }
+  ngOnInit(): void {
+    this.getEmployees();
+  }
+
+    public getEmployees(): void {
+    this.employeeService.getEmployees().subscribe(
+      (response: Employee[]) => {
+        this.employees = response;
+        console.log(this.employees);
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
 }
+    public onUpdateClick(employee:Employee){
+      this.editEmployee=employee;
+    }
+
+    public onDeleteClick(employee:Employee){
+      this.deleteEmployee=employee;
+    }
+
+    public onAddEmployee(addForm:NgForm):void{
+      document.getElementById("add-employee-form")?.click;
+      this.employeeService.addEmployee(addForm.value).subscribe(
+        (response:Employee)=>{this.getEmployees();
+        addForm.reset()},
+        (error:HttpErrorResponse)=>alert(error.message)
+      )
+
+    }
+
+    public onUpdateEmployee(employee:Employee):void{
+      // document.getElementById("add-employee-form")?.click;
+      this.employeeService.updateEmployee(employee).subscribe(
+        (response:Employee)=>this.getEmployees(),
+        (error:HttpErrorResponse)=>alert(error.message)
+      )
+
+    }
+
+    public onDeleteEmployee(id:number):void{
+      // document.getElementById("add-employee-form")?.click;
+      this.employeeService.deleteEmployee(id).subscribe(
+        (response:void)=>this.getEmployees(),
+        (error:HttpErrorResponse)=>alert(error.message)
+      )
+
+    }
+}
+
